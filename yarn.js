b92220cf808e31faa0972e452b645d3844dbe44e
Sam holmes <sam.lb.holmes@gmail.com>
2012-03-30 09:21:37 -0700

    Fixed next. Added a makeNext internal function for multiple this(n)() calls.

--- yarn.js
+++ yarn.js
@@ -1,16 +1,18 @@
 var yarn = function(initial) {
 	var clew = []; // clew is a series of sets of functions [[f, ...], ...]
-	var currentSet = 0;
-	function next(i){
+	function next(i, clewIndex){
 		i = i || 0;
-		return function(){ clew[currentSet++][i].apply(next, arguments); };
+		return function(){ clew[clewIndex][i].apply(makeNext(clewIndex+1), arguments); };
+	}
+	function makeNext(clewIndex){
+		return function(i){ return next(i, clewIndex); };
 	}
 	function add(){
 		clew.push(Array.prototype.slice.call(arguments));
 		return add;
 	}
 	
-	setTimeout(function(){ initial.call(next); }, 0);
+	setTimeout(function(){ initial.call(makeNext(0)); }, 0);
 	return add;
 };
 
