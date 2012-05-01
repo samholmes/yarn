33f4ef9368e5a08a40023123543936428990adc2
Sam holmes <sam.lb.holmes@gmail.com>
2012-04-30 18:50:14 -0700

    Added special error callback; the this.error and yarn(...).error(cb) APIs.

--- yarn.js
+++ yarn.js
@@ -5,12 +5,17 @@ var yarn = function(initial) {
 		return function(){ clew[clewIndex][i].apply(makeNext(clewIndex+1), arguments); };
 	}
 	function makeNext(clewIndex){
-		return function(i){ return next(i, clewIndex); };
+		var f = function(i){ return next(i, clewIndex); };
+		f.error = function(){ if (clew.error) clew.error.apply(null, arguments); };
+		return f;
 	}
 	function add(){
 		clew.push(Array.prototype.slice.call(arguments));
 		return add;
 	}
+	add.error = function(cb){
+		clew.error = cb;
+	};
 	
 	setTimeout(function(){ initial.call(makeNext(0)); }, 0);
 	return add;
